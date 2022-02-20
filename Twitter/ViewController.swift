7b6ff20cbb31f7493a61abffb22ef77546470d16
Vladimir Fibe <motiw@icloud.com>
2022-02-20 20:00:47 +0600

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Vladimir Fibe on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
